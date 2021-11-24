c7eb84c863a53e5a28b946a7d23e727f52e9b035
Vishalkumar Kishanbhai Maheshwari <200465750@student.georgianc.on.ca>
2021-11-24 17:23:29 -0500

    Starting Assignment - 2

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.f21200465750comp2084assignment2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.f21200465750comp2084assignment2 to javafx.fxml;
+    exports com.example.f21200465750comp2084assignment2;
+}
