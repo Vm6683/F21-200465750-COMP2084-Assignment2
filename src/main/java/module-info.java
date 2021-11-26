3b1adbbd8a1881cb9ba1a695965f904b717e7ffb
Vishal Maheshwari <vishalm6683@gmail.com>
2021-11-25 20:06:52 -0500

    Starting Assignment - 2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.example.f21200465750comp2084assignment2 {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.net.http;
+    requires javafx.graphics;
+    requires com.google.gson;
 
     opens com.example.f21200465750comp2084assignment2 to javafx.fxml;
     exports com.example.f21200465750comp2084assignment2;
